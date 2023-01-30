ab95d6abea68ce11b59be75c00ae7f1c9cf30618
Rapto-R <rairplata2@gmail.com>
2023-01-29 18:38:51 -0600

    fixed count

--- react-api-morty/src/components/CharacterList.jsx
+++ react-api-morty/src/components/CharacterList.jsx
@@ -6,12 +6,13 @@ import Character from './Characters';
 import { Dna } from 'react-loader-spinner'
 
 function NavPage(props) {
+  let rest = props.page - 1
   return (
     <header className="d-flex flex-column">
        <h1 className="text-center fw-bold display-1 py-4">Rick & Morty</h1>
       <p className="fw-bold ">Pagina: {props.page}</p>
       <div className="d-flex justify-content-between">
-        <button className="btn btn-primary btn-sm" onClick={() => props.setPage(props.page - 1)}>
+        <button className="btn btn-primary btn-sm" onClick={() => props.setPage(rest == 0 ? 1 : rest )}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-square" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
           </svg>
--- react-api-morty/vite.config.js
+++ react-api-morty/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Rapto-R.github.io/api-react-dinosaurs/",
+  base: "https://Rapto-R.github.io/react-practice/",
   plugins: [react()],
 })
